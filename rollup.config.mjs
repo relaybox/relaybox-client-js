1d971b47b3efc1141b579e0a51a59e894d324c33
Simon Harper <simonharper@Simons-MacBook-Pro.local>
2024-07-13 22:19:59 +0100

    chore: remove es build config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,15 +3,15 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'dist/index.js', // This should be the entry point file compiled by TypeScript
+  input: 'dist/index.js',
   output: [
     {
-      file: 'dist/bundle.cjs.js', // Output file for CommonJS
+      file: 'dist/bundle.cjs.js',
       format: 'cjs',
       sourcemap: false
     },
     {
-      file: 'dist/bundle.esm.js', // Output file for ES Module
+      file: 'dist/bundle.esm.js',
       format: 'esm',
       sourcemap: false
     }
