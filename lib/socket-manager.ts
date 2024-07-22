00a09c41ecbfdaab6048738b16e838aa587858df
livecloud-labs <simonharper@Simons-MacBook-Pro.local>
2024-07-22 21:24:50 +0100

    chore: update connection url

--- lib/socket-manager.ts
+++ lib/socket-manager.ts
@@ -11,7 +11,8 @@ import { logger } from './logger';
 import { ClientEvent, ServerEvent } from './types/event.types';
 import { KeyData, TokenResponse } from './types/request.types';
 
-const UWS_SERVER_HOST = 'http://localhost:9090';
+// const UWS_SERVER_HOST = 'http://localhost:9090';
+const UWS_SERVER_HOST = 'wss://uws.dev.relaybox-services.net';
 const MAX_RECONNECT_ATTEMPTS = 10;
 const INITIAL_RECONNECT_DELAY_MS = 500;
 const MAX_RECONNECT_DELAY_MS = 10000;
