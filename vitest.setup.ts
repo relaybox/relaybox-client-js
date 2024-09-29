c492c5618938f8855ecf900a7250adbb7d0b7de6
livecloud-labs <livecloud.store@gmail.com>
2024-09-29 19:17:44 +0100

    chore: update auth service url

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,2 +1,2 @@
 process.env.UWS_SERVICE_URL = 'wss://localhost:9090/uws/';
-process.env.AUTH_SERVICE_URL = 'http://localhost:9090/auth/dev';
+process.env.AUTH_SERVICE_URL = 'http://localhost:9090/auth';
