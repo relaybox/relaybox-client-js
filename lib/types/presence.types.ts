c6265af993aea957e8ec437ae0d4b571be54eed0
livecloud-labs <livecloud.store@gmail.com>
2024-11-30 11:05:54 +0000

    chore: add presence connection id

--- lib/types/presence.types.ts
+++ lib/types/presence.types.ts
@@ -2,6 +2,7 @@ import { AuthUserPublic } from './auth.types';
 
 export interface PresenceEvent<T = any> {
   clientId: string;
+  connectionId: string;
   timestamp: string;
   data: T;
   event: PresenceEventType;
